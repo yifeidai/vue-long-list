9758870b80aa5f00ff0d88ccd77c216c4323d26b
YifeiDai <2289880474@qq.com>
2019-07-30 18:22:33 +0800

    Init project

---
+++ src/components/list.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="list">
+  <div
+    v-for="item in renderList"
+    class="render-list"
+  >
+    <slot v-bind:item="item"></slot>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  watch: {
+    list() {
+      this.renderList = this.list
+    },
+  },
+
+  data() {
+    return {
+      renderList: [],
+    }
+  },
+
+  created() {
+    this.renderList = this.list
+  },
+}
+</script>
+
+<style lang="stylus">
+.list
+  border: 1px solid black
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <div>vue long list</div>
+
+    <List :list="list">
+      <template v-slot:default="{ item }">
+        <div>
+          <div>{{ item.id }}</div>
+          <div>{{ item.title }}</div>
+        </div>
+      </template>
+    </List>
+  </div>
+</template>
+
+<script>
+import List from "../components/list.vue"
+
+export default {
+  name: 'home',
+
+  data() {
+    return {
+      list: [
+        {
+          id: 1,
+          title: "hello1",
+        },
+        {
+          id: 2,
+          title: "hello2",
+        },
+        {
+          id: 3,
+          title: "hello3",
+        },
+        {
+          id: 4,
+          title: "hello4",
+        },
+        {
+          id: 5,
+          title: "hello5",
+        },
+        {
+          id: 6,
+          title: "hello6",
+        },
+      ],
+    }
+  },
+
+  components: {
+    List,
+  },
+}
+</script>
